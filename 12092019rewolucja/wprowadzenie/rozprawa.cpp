62fe09153241802011194cf9d570247108f9e559
Kacper H. Ikarmus <kacpiczek@gmail.com>
2019-09-12 21:05:15 +0200

    rozprawa vol 1

---
+++ 12092019rewolucja/wprowadzenie/rozprawa.cpp
@@ -0,0 +1,35 @@
+#include "pch.h" 
+#include "èrÛd≥o.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+// (Sprawiedliwizowane) http://www.fupp.org.pl/down/symulacja.pdf  a konkretniej rzecz ujmujπc stπd moøna poúciπgaÊ
+//  nawet øywcem czÍúÊ rozpraw jak na przyk≥ad "Morderstwo w pubie" czy "Wypadek drogowy"
+int rswitch1, rswitch2;
+void rozprawa()
+{
+	// proszÍ powstaÊ przed Wysokim Sπdem (zmienna gracz.imiÍ)
+	// - DziÍkujÍ, proszÍ usiπúÊ. W dniu dzisiejszym przed sπdem odpowiadaÊ bÍdzie (pozwany) w sprawie wytoczonej
+	// przez (powÛd/powÛdka)
+
+	//dobra trutututu nie ja tu od fabu≥y, ktoú coú mÛwi, teraz jako sÍdzia pasowa≥oby dopytaÊ o sprawÍ,
+	//zapoznaÊ siÍ z dowodami i ew. dopytaÊ o coú pozwanego lub úwiadkÛw czy coú
+	cin >> rswitch1;
+	switch (rswitch1)
+	{
+	case 1:
+
+	case 2:
+
+	default:
+		cout << "Podano b≥Ídny numer, ponÛw wybÛr";
+		break;
+	}
+
+}
