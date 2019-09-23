99b2c9cf3ccfc5bab6124297930bec3b77cc316c
KacperH <kahalaczkiewicz@wp.l>
2019-09-23 18:05:14 +0200

    Początek czegoś wielkiego!

---
+++ CSharp_Edition/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CSharp_Edition
+{
+    partial class Rewolucjoniści
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rewolucjoniści));
+            this.SuspendLayout();
+            // 
+            // Rewolucjoniści
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Rewolucjoniści";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
