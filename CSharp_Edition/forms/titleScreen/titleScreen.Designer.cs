e04a943586905f3635754d50832cff97dfc42f16
KacperH <kahalaczkiewicz@wp.l>
2019-09-24 23:04:43 +0200

    Chyba nic nie zepsułem...

--- CSharp_Edition/forms/titleScreen/titleScreen.Designer.cs
+++ CSharp_Edition/forms/titleScreen/titleScreen.Designer.cs
@@ -29,22 +29,24 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rewolucjoniści));
-            this.button1 = new System.Windows.Forms.Button();
+            this.przyciskTytulowy = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // przyciskTytulowy
             // 
-            this.button1.Image = global::CSharp_Edition.Properties.Resources.london_protest03_470x260;
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.przyciskTytulowy.Image = global::CSharp_Edition.Properties.Resources.london_protest03_470x260;
+            resources.ApplyResources(this.przyciskTytulowy, "przyciskTytulowy");
+            this.przyciskTytulowy.Name = "przyciskTytulowy";
+            this.przyciskTytulowy.UseVisualStyleBackColor = true;
+            this.przyciskTytulowy.Click += new System.EventHandler(this.PictureBox1_Click);
             // 
             // Rewolucjoniści
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.przyciskTytulowy);
             this.Name = "Rewolucjoniści";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Rewolucjoniści_Load);
             this.ResumeLayout(false);
 
@@ -52,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button przyciskTytulowy;
     }
 }
 
